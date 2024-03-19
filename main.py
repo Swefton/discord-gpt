53f38629da3b84ae0195ad2aeed2f43e7de5af5c
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2024-03-19 17:37:00 -0400

    working parameters in slash

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ tree = app_commands.CommandTree(client)
     description="My first application Command",
     guild=discord.Object(id=GUILD)
 )
-async def first_command(interaction):
-    await interaction.response.send_message("Hello!")
+async def first_command(interaction, optional_param1 : str = "", optional_param2 : str = ""):
+    await interaction.response.send_message(f"Hello! {optional_param1} {optional_param2}")
+
     
 @client.event
 async def on_ready():
