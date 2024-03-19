2c31959236a181351927c21792804869f2da08cd
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2024-03-19 17:30:08 -0400

    Working Slash Command

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import discord
 from discord import app_commands
 from tokens import TOKEN, GUILD
 
-# setting up the bot
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+# Add the guild ids in which the slash command will appear.
+# If it should be in all, remove the argument, but note that
+# it will take some time (up to an hour) to register the
+# command if it's for all guilds.
 @tree.command(
     name="commandname",
     description="My first application Command",
@@ -14,11 +17,11 @@ tree = app_commands.CommandTree(client)
 )
 async def first_command(interaction):
     await interaction.response.send_message("Hello!")
-
-
+    
 @client.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=GUILD))
     print("Ready!")
-        
-client.run(TOKEN)  
+    
+    
+client.run(TOKEN)
